8ebdc1becabd8546230cfdcd1febf51aa3a84286
jungpa <operland55@naver.com>
2022-07-07 20:42:43 +0900

    NexFlix start

---
+++ src/styled.d.ts
@@ -0,0 +1,18 @@
+import "styled-components";
+
+// and extend them!
+
+declare module "styled-components" {
+	export interface DefaultTheme {
+		red: string;
+		black: {
+			veryDark: string;
+			darker: string;
+			lighter: string;
+		};
+		white: {
+			darker: string;
+			lighter: string;
+		};
+	}
+}
